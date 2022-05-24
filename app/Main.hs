f9a58cfd1584f5f83355fa51819b2401811257f3
DavidGavenda <69786907+DavidGavenda@users.noreply.github.com>
2022-05-24 20:13:02 +0200

    Added progress print

--- app/Main.hs
+++ app/Main.hs
@@ -35,14 +35,23 @@ main = do
     setLocaleEncoding utf8
     file <- openFile "smallCollection.jl" ReadMode
     let emptyMap = Map.empty
+    print "Task 1/10 done"
     let emptyGraph = createEmptyDGraph
+    print "Task 2/10 done"
     (graph, resultIndex) <- createGraph file emptyGraph emptyMap
+    print "Task 3/10 done"
     let edges = stringToMap (Data.Graph.Types.vertices graph)
+    print "Task 4/10 done"
     let numberOfEdges = Data.Graph.Types.order graph
+    print "Task 5/10 done"
     let initialPageRankValues = formatInput numberOfEdges edges
+    print "Task 6/10 done"
     let finalPageRankValues = pageRank edges initialPageRankValues graph 1
+    print "Task 7/10 done"
     let sortedPageRank = sortPageRank (Map.toList finalPageRankValues)
-
+    print "Task 8/10 done"
     createJson sortedPageRank
+    print "Task 9/10 done"
     fileWriteMap resultIndex
+    print "Task 10/10 done"
     hClose file
